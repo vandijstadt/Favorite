869b62a60d0fb9da3ef51ab49a8a4803c9ed1174
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2022-04-20 11:11:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onpointerover=function(){
     for(let i =0; i<element.length;i++){
         element[i].onpointerover=function(){
             console.log('Over :',element[i]);
-            element[i].setAttribute('style', 'background-color: black; border: 3px solid white; padding: 5px; border-radius: 10px;');
         }
         element[i].onpointerout=function(){
             console.log('Left over :',element[i]);
