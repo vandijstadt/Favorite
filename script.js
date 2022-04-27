6a225e3c6c1c78e84923e593e9978ba6e6e1a6f6
Vandijstadt Nicolas <81445233+vandijstadt@users.noreply.github.com>
2022-04-27 09:32:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onpointerover=function(){
     for(let i =0; i<element.length;i++){
         element[i].onpointerover=function(){
             console.log('Over :',element[i]);
-            element[i].setAttribute('style', 'font-size: 50px; background-color: black; border: 3px solid white; padding: 5px; border-radius: 10px;');
+            element[i].setAttribute('style', 'color: black; background-color: white; border: 3px solid black; padding: 5px; border-radius: 10px;');
         }
         element[i].onpointerout=function(){
             console.log('Left over :',element[i]);
